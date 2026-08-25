995d461823ba113fa8c7cf05bf2cf85754e5fdf6
EdgarEldy <ed.eldy21@live.com>
2026-08-25 08:21:55 +0400

    docs(service): add per-method Javadoc to RoleService
    Same as UserService: documents which methods can throw
    ResourceNotFoundException/BusinessRuleException and why.

--- src/main/java/com/edgareldy/springgraphqltutorial/service/RoleService.java
+++ src/main/java/com/edgareldy/springgraphqltutorial/service/RoleService.java
@@ -3,6 +3,8 @@ package com.edgareldy.springgraphqltutorial.service;
 import java.util.List;
 
 import com.edgareldy.springgraphqltutorial.entity.Role;
+import com.edgareldy.springgraphqltutorial.exception.BusinessRuleException;
+import com.edgareldy.springgraphqltutorial.exception.ResourceNotFoundException;
 import com.edgareldy.springgraphqltutorial.graphql.input.RoleInput;
 
 /**
@@ -18,16 +20,34 @@ public interface RoleService {
 
 	List<Role> findAll();
 
+	/**
+	 * @throws ResourceNotFoundException if no role has this id
+	 */
 	Role findById(Long id);
 
+	/**
+	 * @throws BusinessRuleException if the role name is already in use
+	 */
 	Role create(RoleInput input);
 
+	/**
+	 * @throws ResourceNotFoundException if no role has this id
+	 */
 	Role update(Long id, RoleInput input);
 
+	/**
+	 * @throws ResourceNotFoundException if no role has this id
+	 */
 	void delete(Long id);
 
+	/**
+	 * @throws ResourceNotFoundException if the role or the permission does not exist
+	 */
 	Role assignPermissionToRole(Long roleId, Long permissionId);
 
+	/**
+	 * @throws ResourceNotFoundException if the role or the permission does not exist
+	 */
 	Role removePermissionFromRole(Long roleId, Long permissionId);
 
 }
